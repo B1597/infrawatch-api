ba076f2cc9e93a470dc9e04b800e8fe59fff4f87
Bojana Dimovikj <bojana.dimovik@live.com>
2026-04-11 17:26:49 +0200

    expose check-name query params in Swagger

--- src/topology/dto/check-name.dto.ts
+++ src/topology/dto/check-name.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { NodeType } from '../types/node-type.enum';
 
 export class CheckNameDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name!: string;
 
+  @ApiProperty({ enum: NodeType })
   @IsEnum(NodeType)
   type!: NodeType;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   parentId?: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   currentId?: string;
