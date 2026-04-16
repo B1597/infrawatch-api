9a8ecee3b8acdbd5cf68380d7a48b56f4af07d4b
Bojana Dimovikj <bojana.dimovik@live.com>
2026-04-16 14:39:32 +0200

    fix: update spec files to include prisma mock

--- src/topology/topology.service.spec.ts
+++ src/topology/topology.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TopologyService } from './topology.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+const mockPrismaService = {
+  db: {
+    node: {
+      findMany: jest.fn(),
+      findUnique: jest.fn(),
+    },
+  },
+};
 
 describe('TopologyService', () => {
   let service: TopologyService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TopologyService],
+      providers: [
+        TopologyService,
+        { provide: PrismaService, useValue: mockPrismaService },
+      ],
     }).compile();
 
     service = module.get<TopologyService>(TopologyService);
